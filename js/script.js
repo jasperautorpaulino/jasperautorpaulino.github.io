3bd06a18532f9042d4eb64aaf4d6a7c180460d25
jasperautorpaulino <jasper.paulino10@gmail.com>
2020-02-05 12:49:31 +0800

    portfolio third revision

--- js/script.js
+++ js/script.js
@@ -1,29 +1,49 @@
 $(function(){
-	function show(){
-		$(".progressBar").addClass('sticky');
-		$("#topRightLogo").addClass('sticky02');
-	}
-	
-	function hide(){
-		$(".progressBar").removeClass('sticky');
-		$("#topRightLogo").removeClass('sticky02');
-	}
-	
-	$('.progressBar a').click(function(){
-//		var currSec = $(this).attr('href');
-////		$('.secArea section').removeClass('active');
-////		$(currSec).addClass('active');
-		$('.progressBar a i').removeClass('progActive');
-		$(this).children().addClass('progActive');
+
+	$('.card-img-block li').mouseenter(function(){
+		var slicedText = $(this).find('img').attr('src').slice(0, -10);
+		$(this).find('img').attr('src', slicedText + '_white.png')
 	})
 	
+	$('.card-img-block li').mouseleave(function(){
+		var slicedText = $(this).find('img').attr('src').slice(0, -10);
+		$(this).find('img').attr('src', slicedText + '_black.png')
+	})
+
+	$('nav ul li a').on('click', function(){
+		$('nav ul li a').removeClass('active');
+		$(this).addClass('active');
+	})
+
 	$(window).scroll(function(){
-		$('.secArea section').each(function(){
+		$('.sections > div').each(function(){
 			if($(this).offset().top <= $(window).scrollTop() && ($(this).offset().top + $(this).innerHeight()) >= $(window).scrollTop()){
 			var currID = $(this).attr('id');
-			$('.progressBar a i').removeClass('progActive');
-			$('.' + currID).addClass('progActive');
+			$('nav ul li a').removeClass('active');
+			$('.' + currID + 'Nav').addClass('active');
 		}
 		})
+
+		if($(window).scrollTop() >= $('.home').offset().top + $('.home').innerHeight()){
+			$('nav').css('right', '1%')
+		}else{
+			$('nav').css('right', '-100%')
+		}
 	})
-})
+
+	if($(window).scrollTop() >= $('.home').offset().top + $('.home').innerHeight()){
+		$('nav').css('right', '1%')
+	}else{
+		$('nav').css('right', '-100%')
+	}
+
+
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true
+	})
+
+	const observer = lozad();
+	observer.observe();
+});
